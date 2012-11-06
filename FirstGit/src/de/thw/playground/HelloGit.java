1127dc39de74cadc567ab4bd2be0428c85ab5bc0
Thomas Weinstein <Thomas.Weinstein@ict-se.de>
2012-11-06 16:22:14 +0100

    commented

--- FirstGit/src/de/thw/playground/HelloGit.java
+++ FirstGit/src/de/thw/playground/HelloGit.java
@@ -2,6 +2,10 @@ package de.thw.playground;
 
 public class HelloGit
 {
+    /**
+     * Only to try out GIT
+     * @param args
+     */
     public static void main( String[] args )
     {
         System.out.println( "Hello GIT" );
